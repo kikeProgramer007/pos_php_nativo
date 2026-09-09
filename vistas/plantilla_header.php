ed8093620ecbf665ac0f2933f51c2c5cb2ea972a
marcosgalarza1 <luismarcogalarza10@gmail.com>
2026-09-08 20:17:20 -0400

    img

--- vistas/plantilla_header.php
+++ vistas/plantilla_header.php
@@ -8,7 +8,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>El Gato Rico -Churrasqueria</title>
+  <title>Pollos 360</title>
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
