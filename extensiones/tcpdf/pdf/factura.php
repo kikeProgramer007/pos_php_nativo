5e691535532d281cb88b8b7a510fc502f30c9aa6
marcosgalarza1 <luismarcogalarza10@gmail.com>
2026-09-13 13:48:09 -0400

    Corregir el formato del mensaje de agradecimiento en las facturas para mantener la consistencia en la presentación

--- extensiones/tcpdf/pdf/factura.php
+++ extensiones/tcpdf/pdf/factura.php
@@ -242,7 +242,7 @@ class imprimirFactura
             </tr>
             </tbody>
         </table>
-         <p style="font-size: 9px; text-align: center;">¡GRACIAS POR SU COMPRA!<br>Sistemas al 73982982</p>
+         <p style="font-size: 9px; text-align: center;">¡GRACIAS POR SU COMPRA!<br>SISTEMAS AL 73982982</p>
         ';
 
         $pdfFactura->writeHTML($htmlFactura, false, false, false, false, '');
